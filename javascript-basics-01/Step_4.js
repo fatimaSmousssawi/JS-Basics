f46f2eb4c52541b0cba5dc12f6f27b01850668c3
fatimaSmoussawi <fatimasamoussawi@gmail.com>
2020-09-30 12:52:32 +0300

    completed-step_4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,5 @@
+var name=prompt("Please enter your name");
+var surname=prompt("Please enter your surname");
+var city=prompt("Please enter your city");
+alert("Nom: "+name+"\n"+ "Pernom: " +surname+"\n" +"Ville: " + city )
+alert("Hello "+name);
