8517f825a9925d3aec4ebfa6a8f5e8a0f08e3481
fatimaSmoussawi <fatimasamoussawi@gmail.com>
2020-10-01 16:15:56 +0300

    completed-step3

---
+++ javascript-basics-03/step 2/step2.js
@@ -0,0 +1,6 @@
+var a= document.getElementById("hide");
+var c= document.getElementById("texte");
+var b= document.getElementById("show");
+
+a.addEventListener("click",function(){  c.style.display="none"  });
+b.addEventListener("click",function(){  c.style.display="block"  });
